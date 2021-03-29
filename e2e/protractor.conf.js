aeaf9362e83dde06596d6957e4a97839174aab17
Zeeshan Iqbal <zeeshan.iqbal.cs@gmail.com>
2021-03-30 03:30:44 +0500

    a bare minimum website is up

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -17,7 +17,7 @@ exports.config = {
   },
   directConnect: true,
   SELENIUM_PROMISE_MANAGER: false,
-  baseUrl: 'http://localhost:8080/',
+  baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
